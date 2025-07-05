dbe9a98a767e6e820e787c75aadd641b0a652321
Volodymyr <sainchukvova2104@gmail.com>
2025-07-05 13:46:39 +0300

    fix 9

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,9 +1,23 @@
-export default interface Movie {
-  id: number;
-  poster_path: string;
-  backdrop_path: string;
-  title: string;
-  overview: string;
-  release_date: string;
-  vote_average: number;
+import axios from "axios";
+import type Movie from "../types/movie";
+
+// Базова конфігурація axios
+const axiosInstance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+  },
+});
+
+/**
+ * Пошук фільмів за ключовим словом
+ * @param query - текст запиту
+ * @returns масив фільмів
+ */
+export async function searchMovies(query: string): Promise<Movie[]> {
+  const response = await axiosInstance.get("/search/movie", {
+    params: { query },
+  });
+
+  return response.data.results ?? [];
 }
